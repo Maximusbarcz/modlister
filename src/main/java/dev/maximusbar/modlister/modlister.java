39053499f9c6b9efee425acfbac396ef2635bb73
Maximusbarcz <maxim.baranek@gmail.com>
2022-08-25 22:11:08 +0200

    some shit

--- src/main/java/dev/maximusbar/modlister/modlister.java
+++ src/main/java/dev/maximusbar/modlister/modlister.java
@@ -1,30 +1,53 @@
 package dev.maximusbar.modlister;
 
 import java.io.*;
+import java.util.Enumeration;
 import java.util.Scanner;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public class modlister {
-    public static void main(String[] args) throws IOException {
-        String[] modNames;
-        Scanner sc = new Scanner(System.in);
+    public static class ListJar {
+        private static void process(InputStream input) throws IOException {
+            InputStreamReader isr = new InputStreamReader(input);
+            BufferedReader reader = new BufferedReader(isr);
+            String line;
 
-        System.out.println("Please enter your desired file path below:");
-        String filePath = sc.nextLine();
-        File f = new File(filePath);
-
-        modNames = f.list();
-
-        System.out.println("List of mods: ");
-        for (String modName : modNames) {
-            System.out.println(modName);
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("  \"id\": ")) {
+                    System.out.println(line.replace(  "  \"id\": \"", "").replace("\",", ""));
+                }
+            }
+            reader.close();
         }
-        System.out.println("Exported "+ modNames.length + " mods to mods.txt");
-        FileWriter fw = new FileWriter(new File(filePath, "mods.txt"));
-        for(String str: modNames) {
-            fw.write(str.replaceAll(".jar", "") + System.lineSeparator());
+
+        public static void main(String arg[]) throws IOException {
+            String jarPath;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Please enter your desired file path below:");
+            String filePath = sc.nextLine();
+            File f = new File(filePath);
+            String[] modNames;
+            modNames = f.list();
+            for (String modName : modNames) {
+                System.out.println(modName);
+                jarPath = filePath + "\\" + modName;
+                JarFile modJar = new JarFile(jarPath);
+                final Enumeration<JarEntry> entries = modJar.entries();
+                while (entries.hasMoreElements()) {
+                    final JarEntry entry = entries.nextElement();
+                    System.out.println("File : " + entry.getName());
+                    if (entry.getName().contains("fabric.mod.json")) {
+                        JarEntry fileEntry = modJar.getJarEntry(entry.getName());
+                        InputStream input = modJar.getInputStream(fileEntry);
+                        process(input);
+                        break;
+                    }
+                }
+
+            }
+            sc.close();
         }
-        fw.close();
-        sc.close();
     }
 }
 
